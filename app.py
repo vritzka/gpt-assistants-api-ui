4faf3ff1af3bf84585b7bc3473a9cb713129539f
Volker <vritzka@gmail.com>
2024-09-17 03:08:45 +0000

    when initial greeting is empty don't show it

--- app.py
+++ app.py
@@ -60,8 +60,10 @@ if 'openai_api_key' not in st.session_state:
 
     if 'greeted' not in st.session_state:
         st.session_state['greeted'] = True
-        with st.chat_message("assistant"):
-            st.write(initial_greeting)
+        if initial_greeting:
+            with st.chat_message("assistant"):
+                st.write(initial_greeting)
+                
 # Load environment variables
 instructions = os.environ.get("RUN_INSTRUCTIONS", "Instructions")
 enabled_file_upload_message = os.environ.get(
